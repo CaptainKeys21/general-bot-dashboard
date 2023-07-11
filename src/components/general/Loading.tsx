d288699a73691f6a75738030e8b957f5bea1d0db
CaptainKeys21 <CaptainKeys21@github.com>
2023-07-11 00:32:00 -0300

    Member search

---
+++ src/components/general/Loading.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import { ImSpinner2 }from 'react-icons/im';
+
+const Loading = () => {
+  return(
+    <div className='loading-wrapper'>
+      <ImSpinner2 />
+    </div>
+  );
+};
+
+export default Loading;
